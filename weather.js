c6ac84a26338e3a7d8ee9b617166887109f00950
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-03-16 21:29:37 -0500

    added some js

--- weather.js
+++ weather.js
@@ -1,16 +1,37 @@
 currentTime = $("#current-time").text(moment().format("L"));
-userInput = $("#user-input").val();
+currentCity = $(".user-input1").val();
+
 submitBtn = $(".search-city");
 cityList = $(".input-group-append");
-submitBtn.addEventListener("click", searchCity);
 
-$.ajax(
-  "https://api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}"
-).done(function (data) {
-  console.log(data);
+$(document).ready(function () {
+  $(".search-city").on("click", searchCity);
 });
+
+var getCity = function (user) {
+  // format the github api url
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    "austin" +
+    "&appid=37e6dcaaa7be78a19d983eb490d52ae4";
+
+  // make a get request to url
+  fetch(apiUrl).then(function (response) {
+    console.log(response);
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+getCity();
 
 var searchCity = function (event) {
   event.preventDefault();
-  console.log(event);
+  searchCity = console.log(event);
+
+  if (currentCity) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  }
 };
